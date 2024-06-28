c2f1b3fff6afe69ea9aba4d5f4da0b26dae75a16
Raphael <raphael.kropp@web.de>
2024-06-28 19:52:32 +0200

    map functions

--- typo3conf/ext/ods_osm/Resources/Public/JavaScript/Leaflet/leaflet-geosearch/geosearchInit.js
+++ typo3conf/ext/ods_osm/Resources/Public/JavaScript/Leaflet/leaflet-geosearch/geosearchInit.js
@@ -4,6 +4,8 @@ const search = new GeoSearch.GeoSearchControl({
     notFoundMessage: 'Ihre eingegebene Adresse wurde leider nicht gefunden.',
     style: 'bar',
     searchLabel: 'Adresse eingeben',
+    keepResult: true,
+    updateMap: false,
     marker: {
         // optional: L.Marker    - default L.Icon.Default
         icon: new L.Icon.Default(),
@@ -13,9 +15,9 @@ const search = new GeoSearch.GeoSearchControl({
 
 
 let newLocation;
-
-const lat1 = 49.74606765;
-const lon1 = 6.68718716;
+// Uni Trier
+const latUni = 49.74606765;
+const lonUni = 6.68718716;
 
 
 function toRadians(degrees) {
@@ -55,7 +57,7 @@ function formatDistanceString(distance, name) {
             break;
         case distance > 200.0:
             text += 'Ich würde herziehen. Die Universität ist top und du lernst hier sicher schnell viele Freunde kennen.' +
-                ' Trier ist ohnehin eine sehr schöne und die älteste Stadt deutschlands.'
+                ' Trier ist ohnehin eine sehr schöne und die älteste Stadt Deutschlands.'
             break;
         case distance > 50.0:
             text += 'Trier ist cool. Eine Wohnung in Trier lohnt sich. Am Wochenden kannst du deine Freunde und Familie zu Hause treffen. ' +
@@ -63,7 +65,7 @@ function formatDistanceString(distance, name) {
             break;
         case distance > 10.0:
             text += 'Das ist gar nicht so weit weg. Du kannst bei deinen Eltern wohnen und trotzdem in Trier studieren. ' +
-                'Oder du ziehst gleich in die älteste Stadt deutschlands. Am besten sofort einschreiben.'
+                'Oder du ziehst gleich in die älteste Stadt Deutschlands. Am besten sofort einschreiben.'
             break;
         case distance <= 10.0:
             text += 'Du wohnst ja quasi in Trier. Schreibe dich am besten gleich ein.'
@@ -74,7 +76,11 @@ function formatDistanceString(distance, name) {
     return text
 }
 
+let line
 function processLocation(ev) {
+    if (line != null) {
+        osm_347.removeLayer(line)
+    }
     let locationDistanceElement = document.querySelector('#locationDistance');
     newLocation = ev;
     lon2 = ev['location']['x'];
@@ -82,20 +88,26 @@ function processLocation(ev) {
     let name = newLocation['location']['label'];
     console.log(newLocation);
     // Distanz berechnen
-    let distance = haversine(lat1, lon1, lat2, lon2);
+    let distance = haversine(latUni, lonUni, lat2, lon2);
     locationDistanceElement.innerHTML = formatDistanceString(distance, name)
+
+    let latlngs = [
+        [latUni, lonUni],
+        [lat2, lon2]
+    ]
+    line = L.polyline(latlngs, {color: '#007AC3'}).addTo(osm_347);
+    osm_347.fitBounds(line.getBounds());
 }
 
 function createUniPin() {
-
     let UniPin = L.icon({
         iconUrl: '/fileadmin/logo/LogoPin.png',
 
         iconSize: [38, 61],
-        iconAnchor: [22, 60],
-        popupAnchor: [-3, -50]
+        iconAnchor: [36, 60],
+        popupAnchor: [-17, -50]
     });
-    L.marker([lat1, lon1], {icon: UniPin}).addTo(osm_347).bindPopup("Universität Trier");
+    L.marker([latUni, lonUni], {icon: UniPin}).addTo(osm_347).bindPopup("Universität Trier");
 }
 
 function initGeosearch() {
