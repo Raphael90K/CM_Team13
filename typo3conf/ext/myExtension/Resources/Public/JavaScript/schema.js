30f5fe4cdc8feb0fbe6e3c9d5d2f218ca160d959
Raphael <raphael.kropp@web.de>
2024-07-09 23:00:30 +0200

    mask templates

---
+++ typo3conf/ext/myExtension/Resources/Public/JavaScript/schema.js
@@ -0,0 +1,23 @@
+const schemaJSON = {
+    "@context": "https://schema.org",
+    "@type": "CollegeOrUniversity",
+    "name": "Universität Trier",
+    "url": "https://www.uni-trier.de/",
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Universitätsring 15",
+        "addressLocality": "Trier",
+        "postalCode": "54296",
+        "addressCountry": "Deutschland"
+    },
+    "telephone": "+49 651 201-0",
+    "email": "info@uni-trier.de",
+    "sameAs": [
+        "https://www.facebook.com/uni.trier",
+        "http://www.twitter.com/trieruni",
+        "https://www.instagram.com/uni_trier/",
+        "https://www.linkedin.com/school/trieruni/",
+        "https://www.youtube.com/unitrier"
+    ]
+}
+
---
+++ typo3conf/ext/myExtension/ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "bootstrap_package".
+ *
+ * Auto generated 22-05-2024 19:24
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'myExtension',
+  'description' => 'myExtension',
+  'category' => 'templates',
+  'version' => '1.0.0',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Raphael Kropp',
+  'author_email' => 's4rakrop@uni-trier.de',
+  'author_company' => 'private',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '11.5.0-12.4.99'
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
